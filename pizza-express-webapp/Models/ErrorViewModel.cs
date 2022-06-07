1c365ef28afe8e03445bc4062d196b6cd8c9bd6e
Kashish Madan <kashishmadan@microsoft.com>
2022-06-07 14:25:12 +0530

    Add project files.

---
+++ pizza-express-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pizza_express_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
